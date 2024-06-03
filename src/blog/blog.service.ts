230ebbe2fc43473b594b8e523451c2a8780b45ad
Yoann-Michon <yoann972.wm@outlook.com>
2024-06-03 13:39:35 +0200

    bug create

--- src/blog/blog.service.ts
+++ src/blog/blog.service.ts
@@ -90,13 +90,6 @@ async function scrapeArticle(url: string): Promise<{ title: string, content: str
 
     browser = await puppeteer.launch({
       headless: true,
-      args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-dev-shm-usage',
-        '--disable-accelerated-2d-canvas',
-        '--disable-gpu'
-      ]
     });
     console.log('-------------------------------------------------------------------------')
     console.log('before page')
